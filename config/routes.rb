41560545f9313f1dfd76197dda4d1d231e93a1dd
rodrigotello <rodrigotello@outlook.com>
2013-06-04 17:25:42 -0500

    CSS added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Pizzasapp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
+ root :to => 'static_pages#home'
+  
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
