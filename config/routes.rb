ac995f9bad3fbb4dae4b487014c812bf9d7721e7
rodrigotello <rodrigotello@outlook.com>
2013-06-05 00:13:48 -0500

    Solving fucking problems

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Pizzasapp::Application.routes.draw do
-  root :to => 'static_pages#home'
-
-  get "static_pages/help"
-  get "static_pages/about"
-
+ root :to => 'static_pages#home'
+  
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
